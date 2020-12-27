39ac5148a156a9c5a4e389929785a4d902af58d9
lucho025 <59146593+lucho025@users.noreply.github.com>
2020-12-27 16:39:52 -0300

    Desafio 2 - Luis Garcia
    Desafio 2 - Luis Garcia
    Lista

---
+++ src/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function App (){
+    return(
+        <header>
+            <h1>Tienda de E-Commerce - Distrimundo</h1>
+            <ul>
+            <li><a href="#" target="_blank">Inicio</a></li>
+            <li><a href="#" target="_blank">Productos</a></li>
+            <li><a href="#" target="_blank">Formas de Pago</a></li>
+            <li><a href="#" target="_blank">Metodos de Envio</a></li>
+            <li><a href="#" target="_blank">Contacto</a></li>
+            </ul>
+        </header>
+    )
+}
+
+export default App 
