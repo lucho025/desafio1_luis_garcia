8d035f5f994059d196fc0cd0fd645576f3fc3b6f
lucho025 <59146593+lucho025@users.noreply.github.com>
2020-12-31 18:48:09 -0300

    Desafio 2 - Luis Garcia
    Desafio 2 - Luis Garcia - Reentrega

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import NavBar from './components/NavBar.js'
+
+
+
+const App = () => {
+    return (
+        <>
+            <NavBar/>
+            
+        </>
+    )
+}
+
+export default App
