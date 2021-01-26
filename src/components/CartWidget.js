363d26a1457765b41cad5983ba40324e62e9c68d
lucho025 <59146593+lucho025@users.noreply.github.com>
2021-01-25 22:20:08 -0300

    Desafio 10 - Luis Garcia
    Desafio 10 - Luis Garcia

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { CartContext } from './CartContext';
 
-const CartWidget = () => {
+
+function CartWidget() {
+    const { quantity } = useContext(CartContext)
+
     return (
         <>
+             { quantity > 0 && 
             <p align="center">
                 <p></p> 
 {/*                 <img src="https://images.bidcom.com.ar/resize?src=https://www.bidcom.com.ar/publicacionesML/productos/PROJ0046/1000x1000-PROJ0046.jpg&w=500&q=100" height="100px"></img>
  */}                <br></br>
                 <i class='fas fa-cart-arrow-down' style={{fontSize: 40}}></i>
+                <h4>Usted posee {quantity} articulos agregados al carrito</h4>
 
-            </p>
+            </p>}
         </>
     )
 }
